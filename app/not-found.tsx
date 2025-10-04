8016294235dd57c36a3b13fb9965651eae8e4041
Youky <rizfirsy@gmail.com>
2025-10-04 19:54:24 +0700

    drafting 404

---
+++ app/not-found.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import Image from "next/image";
+import PrimaryButton from "./components/PrimaryButton";
+import SecondaryButton from "./components/SecondaryButton";
+
+export default function NotFound() {
+  return (
+    <section className="h-[60vh] py-20 flex flex-col items-center">
+      <div className="flex flex-col gap-8 h-full w-full items-center justify-center">
+        <div className="flex items-center gap-8">
+          <Image
+            src="/devfest/4_green.svg"
+            alt="arrow up"
+            width={150}
+            height={150}
+            className="hover:-scale-100 transition-all duration-700 grayscale hover:grayscale-0"
+          />
+          <Image
+            src="/devfest/globe_monoline.svg"
+            alt="arrow up"
+            width={150}
+            height={150}
+          />
+          <Image
+            src="/devfest/4_blue.svg"
+            alt="arrow up"
+            width={150}
+            height={150}
+            className="hover:-scale-100 transition-all duration-700 grayscale hover:grayscale-0"
+          />
+        </div>
+        <h2 className="text-center">It seems you got a little bit lost</h2>
+        <div className="flex items-center justify-center gap-4">
+          <SecondaryButton destination="/hackfest">
+            Visit Hackfest
+          </SecondaryButton>
+          <PrimaryButton destination="/">Back to Home</PrimaryButton>
+        </div>
+      </div>
+    </section>
+  );
+}
