82825631c425d4606f3e32528561d79f6fb7a4ef
Youky <rizfirsy@gmail.com>
2025-10-05 19:30:30 +0700

    Redesign footer section

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,28 +1,49 @@
 import React from "react";
 import PrimaryButton from "./PrimaryButton";
 import Logo from "./Logo";
+import { url } from "inspector";
+import Link from "next/link";
+import { Facebook, Instagram, Youtube } from "lucide-react";
 
 const Footer = () => {
+  const navigationMenu = [
+    { title: "Details", url: "#about", isSpecial: false },
+    { title: "Speaker", url: "#speakers", isSpecial: false },
+    { title: "Schedule", url: "#schedule", isSpecial: false },
+    { title: "Venue", url: "#venue" },
+    // { title: "Hackfest", url: "/hackfest", isSpecial: true }, hidden until we determine the date
+  ];
+
   return (
-    <footer className="p-4">
-      <div className="grid grid-cols-1 md:grid-cols-2 py-8 gap-8">
-        <div>
-          <h2 className="mb-4">Learn, Connect, and Grow.</h2>
-          <PrimaryButton destination="#" className="w-fit">
-            Secure your seat
-          </PrimaryButton>
-        </div>
-        <div className="grid gap-4">
+    <footer className="py-20 px-4">
+      <div className="grid place-content-center gap-8">
+        <div className="grid place-content-center">
           <Logo />
-          <p className="text-sm">
-            An extraordinary event where enthusiasts and experts meet to share
-            their passion for development across the board.
-          </p>
         </div>
-      </div>
-      <div className="flex justify-between items-center py-4 border-t border-zinc-200">
-        <p>Copyright © DevFest Jakarta 2025</p>
-        <p>Made with love by GDG Jakarta</p>
+        <ul className="flex items-center gap-8 justify-center">
+          {navigationMenu.map((menu, index) => (
+            <li key={index}>
+              <Link href={menu.url}>{menu.title}</Link>
+            </li>
+          ))}
+        </ul>
+        <ul className="flex items-center gap-8 justify-center">
+          <li>
+            <Link href="https://www.instagram.com/gdgjakarta/">
+              <Instagram />
+            </Link>
+          </li>
+          <li>
+            <Link href="https://www.facebook.com/gdgjakarta/">
+              <Facebook />
+            </Link>
+          </li>
+          <li>
+            <Link href="https://www.youtube.com/gdgjakarta/">
+              <Youtube />
+            </Link>
+          </li>
+        </ul>
       </div>
     </footer>
   );
