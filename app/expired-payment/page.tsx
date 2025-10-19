4e2490bf9fed6009a8fc3ef96dd61f85d689c9eb
Youky <rizfirsy@gmail.com>
2025-10-19 12:30:37 +0700

    create expired payment page and update the logo

---
+++ app/expired-payment/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import React from "react";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+import PrimaryButton from "../components/PrimaryButton";
+
+const ExpiredPayment = () => {
+  return (
+    <section className="h-[60vh] py-20">
+      <div className="max-w-3xs -translate-x-1/4">
+        <DotLottieReact src="failed.lottie" loop autoplay />
+      </div>
+      <div className="flex flex-col gap-4 md:gap-8">
+        <h1>Oppsyy! </h1>
+        <h3>Your payment link was expired.</h3>
+        <div>
+          <PrimaryButton destination="https://www.instagram.com/gdgjakarta/">
+            Contact us for help
+          </PrimaryButton>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ExpiredPayment;
